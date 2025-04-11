8e41543d25d9fb27ec49c72335f532210dce1979
yvinc <imvincy@outlook.com>
2025-04-11 00:30:05 -0700

    fix running error for dependencies problem for pairsplot function

--- R/eda_viz_pairplot.R
+++ R/eda_viz_pairplot.R
@@ -7,16 +7,20 @@
 #' @param width Width of the plot in inches (default: 16).
 #' @param height Height of the plot in inches (default: 12).
 #' @return None (saves the plot to a file).
+#' @importFrom GGally ggpairs
+#' @importFrom ggplot2 ggsave theme ggtitle element_text aes
 #' @examples
-#' data <- data.frame(a = 1:3, b = 4:6)
-#' create_pairs_plot(data, "pairs_plot.png")
+#' data <- data.frame(a = rnorm(10), b = rnorm(10))
+#' temp_file <- tempfile(fileext = ".png")
+#' on.exit(unlink(temp_file))  # this cleans up the file after the example runs
+#' create_pairs_plot(data, temp_file)
 create_pairs_plot <- function(data, output_file, width = 16, height = 12) {
   options(repr.plot.width = width, repr.plot.height = height)
-  
-  plot <- ggpairs(data, aes(alpha = 0.5)) +
-    theme(text = element_text(size = 13)) +
-    ggtitle("Pairs plot for all variables of interest for exploration") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
-  
-  ggsave(output_file, plot)
+
+  plot <- GGally::ggpairs(data, aes(alpha = 0.5)) +
+    ggplot2::theme(text = element_text(size = 13)) +
+    ggplot2::ggtitle("Pairs plot for all variables of interest for exploration") +
+    ggplot2::theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+  ggplot2::ggsave(output_file, plot)
 }
